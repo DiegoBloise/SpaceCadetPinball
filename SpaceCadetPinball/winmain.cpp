3859f1b82b96c0ba378991fa75fb2073fbfd0334
Diego Bloise <diego.bloise@gmail.com>
2026-07-16 13:39:19 -0300

    feat: add PSP platform support to CMakeLists and winmain

--- SpaceCadetPinball/winmain.cpp
+++ SpaceCadetPinball/winmain.cpp
@@ -12,6 +12,14 @@
 #include "translations.h"
 #include "font_selection.h"
 
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspdisplay.h>
+
+// PSP_MODULE_INFO is required
+PSP_MODULE_INFO("Hello World", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
+
 constexpr const char* winmain::Version;
 
 SDL_Window* winmain::MainWindow = nullptr;
@@ -52,8 +60,28 @@ winmain::DurationMs winmain::SpinThreshold = DurationMs(0.005);
 WelfordState winmain::SleepState{};
 int winmain::CursorIdleCounter = 0;
 
+int exit_callback(int arg1, int arg2, void *common) {
+    sceKernelExitGame();
+    return 0;
+}
+
+int callback_thread(SceSize args, void *argp) {
+    int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+    sceKernelRegisterExitCallback(cbid);
+    sceKernelSleepThreadCB();
+    return 0;
+}
+
+int setup_callbacks(void) {
+    int thid = sceKernelCreateThread("update_thread", callback_thread, 0x11, 0xFA0, 0, 0);
+    if(thid >= 0)
+        sceKernelStartThread(thid, 0, 0);
+    return thid;
+}
+
 int winmain::WinMain(LPCSTR lpCmdLine)
 {
+  setup_callbacks();
 	std::set_new_handler(memalloc_failure);
 
 	printf("Game version: %s\n", Version);
